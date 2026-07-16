fae9907aa8dfc47a1acba3cc231b7b25d7a31860
sasha <alexcontact793@gmail.com>
2026-07-16 20:07:17 +0300

    add: refresh token lifetime

--- backend/_settings/settings.py
+++ backend/_settings/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(os.environ.get('JWT_ACCESS_TOKEN_LIFETIME_MINUTES', 60))),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(os.environ.get('JWT_REFRESH_TOKEN_LIFETIME_DAYS', 1))),
 }
 
 AUTHENTICATION_BACKENDS = [
